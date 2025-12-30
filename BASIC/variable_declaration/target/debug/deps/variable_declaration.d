57dae69e7ae54ff873401e4dd10278adbb862ef6
sairam208 <sairam2006soramesh@gmail.com>
2025-12-30 14:31:24 +0530

    A new folder variable declaration is added

---
+++ BASIC/variable_declaration/target/debug/deps/variable_declaration.d
@@ -0,0 +1,5 @@
+G:\EMBEDDED SYSTEM\RUST PROGRAMMING\BASIC\variable_declaration\target\debug\deps\variable_declaration.d: src\main.rs
+
+G:\EMBEDDED SYSTEM\RUST PROGRAMMING\BASIC\variable_declaration\target\debug\deps\variable_declaration.exe: src\main.rs
+
+src\main.rs:
---
+++ BASIC/variable_declaration/target/debug/variable_declaration.d
@@ -0,0 +1,1 @@
+G:\EMBEDDED\ SYSTEM\RUST\ PROGRAMMING\BASIC\variable_declaration\target\debug\variable_declaration.exe: G:\EMBEDDED\ SYSTEM\RUST\ PROGRAMMING\BASIC\variable_declaration\src\main.rs
